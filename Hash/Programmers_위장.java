32b52fe7a2839206597ddd870551bfded197b1c0
yongguk <yonguk929@gmail.com>
2022-04-04 23:30:32 +0900

    Create Programmers_위장.java

---
+++ Hash/Programmers_위장.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for(int i = 0; i < clothes.length; i++){
+            if(map.containsKey(clothes[i][1]));
+                map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1) + 1);
+        }
+        answer = 1;
+        for(String key : map.keySet()){
+            answer *= map.get(key);
+        }
+        return answer-1;
+    }
+}
