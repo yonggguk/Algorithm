e77a406f2f7b3790ec5d0b727ac8248e55694cec
yongguk <yonguk929@gmail.com>
2022-04-12 22:55:20 +0900

    Update 신고받기.py

--- Kakao/신고받기.py
+++ Kakao/신고받기.py
@@ -1,6 +1,20 @@
+from collections import defaultdict
+
 def solution(id_list, report, k):
     answer = []
     report = set(report)
-    temp_list = []
-    print(report)
+    reporter_dict = defaultdict(set)
+    busted_dict = defaultdict(set)
+    
+    for i in report:
+        reporter, busted = i.split()
+        reporter_dict[reporter].add(busted)
+        busted_dict[busted].add(reporter)
+    for _id in id_list:
+        cnt = 0
+        for j in reporter_dict[_id]:
+            if len(busted_dict[j]) >= k:
+                cnt += 1
+        answer.append(cnt)
+    
     return answer
