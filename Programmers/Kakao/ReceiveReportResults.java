17008c32b59ce0d1a3d9a855fb3fe609cafd7793
yongguk <yonguk929@gmail.com>
2022-05-28 23:09:15 +0900

    Create ReceiveReportResults.java

---
+++ Programmers/Kakao/ReceiveReportResults.java
@@ -0,0 +1,19 @@
+/*
+2022 KAKAO BLIND RECRUITMENT
+신고 결과 받기
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = {};
+        String s = new String("1");
+        String s1 = new String("1");
+        System.out.println(s == s1);
+        System.out.println(System.identityHashCode(s));
+        System.out.println(System.identityHashCode(s1));
+        
+        return answer;
+    }
+}
