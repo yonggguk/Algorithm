927e25602e964cbbbcb76fcb9c4197cbdc833677
yongguk <yonguk929@gmail.com>
2022-06-07 23:01:16 +0900

    Update ReceiveReportResults.java

--- Programmers/Kakao/ReceiveReportResults.java
+++ Programmers/Kakao/ReceiveReportResults.java
@@ -7,13 +7,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
-        String s = new String("1");
-        String s1 = new String("1");
-        System.out.println(s == s1);
-        System.out.println(System.identityHashCode(s));
-        System.out.println(System.identityHashCode(s1));
+        int[] answer = new int[id_list.length];
+        Map<String, Integer> index = new HashMap<>();
+        Map<String, List<Integer> list = new HashMap<>();
         
-        return answer;
+        for(int i = 0; i < id_list.length; i++){
+            index.put(id_list[i], i);
+        }
+        
+        for(String rep : report){
+            String[] ids = rep.split(" ");
+            String fromId = ids[0], toId=ids[1];
+            if(!list.containKey(toId)) list.put(toId, new ArrayList<>());
+            List<Integer> tmp = list.get(toId);
+        }
     }
 }
