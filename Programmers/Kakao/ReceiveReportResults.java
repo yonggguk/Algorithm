f7b3c207601c80dc3f92d1cf2fcc0df65a25cdc8
yongguk <yonguk929@gmail.com>
2022-06-08 22:17:40 +0900

    Update ReceiveReportResults.java

--- Programmers/Kakao/ReceiveReportResults.java
+++ Programmers/Kakao/ReceiveReportResults.java
@@ -9,7 +9,7 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         Map<String, Integer> index = new HashMap<>();
-        Map<String, List<Integer> list = new HashMap<>();
+        Map<String, List<Integer>> list = new HashMap<>();
         
         for(int i = 0; i < id_list.length; i++){
             index.put(id_list[i], i);
@@ -18,8 +18,26 @@ class Solution {
         for(String rep : report){
             String[] ids = rep.split(" ");
             String fromId = ids[0], toId=ids[1];
-            if(!list.containKey(toId)) list.put(toId, new ArrayList<>());
+            if(!list.containsKey(toId)){
+                list.put(toId, new ArrayList<>());
+            }
             List<Integer> tmp = list.get(toId);
+            if(!tmp.contains(index.get(fromId))){
+                tmp.add(index.get(fromId));
+            }
+            System.out.println(System.identityHashCode(tmp));
+            System.out.println(System.identityHashCode(list.get(toId)));
         }
+
+        for(int i = 0; i < id_list.length; i++){
+            String id = id_list[i];
+            System.out.println(id);
+            if(list.containsKey(id) && list.get(id).size() >= k){
+                for(int idx : list.get(id)){
+                    answer[idx]++;
+                }
+            }
+        }
+        return answer;
     }
 }
