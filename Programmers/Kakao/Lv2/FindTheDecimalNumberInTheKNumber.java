f5ed1ed6d8facf96a0f7639bf68bc4e12f2acf34
yonggguk <yonguk929@gmail.com>
2022-06-10 20:05:30 +0900

    Add files

--- Programmers/Kakao/Lv2/FindTheDecimalNumberInTheKNumber.java
+++ Programmers/Kakao/Lv2/FindTheDecimalNumberInTheKNumber.java
@@ -1,9 +1,24 @@
 package Lv2;
 
 import java.util.Scanner;
+import java.lang.Math;
+import java.util.Arrays;
 
 public class FindTheDecimalNumberInTheKNumber {
-    public static String k_number(int n, int k){
+    public boolean is_prime(long n){
+        if( n <= 1 ) return false;
+        else if( n == 2 ) return true;
+        else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public String k_number(int n, int k){
         String stringN = Integer.toString(n);
         StringBuilder sb = new StringBuilder();
         if(k == 10){
@@ -20,21 +35,31 @@ public class FindTheDecimalNumberInTheKNumber {
         return sb.reverse().toString();
     }
 
-    public static int solution(int n, int k){
-        int answer = -1;
+    public int solution(int n, int k){
+        int answer = 0;
+        String k_num = k_number(n, k);
+        String[] moNum = k_num.split("0");
+        Long caseNum;
+        for(int i = 0; i < moNum.length; i++){
+            if(moNum[i] == "") continue;
+            caseNum = Long.parseLong(moNum[i]);
+            if(is_prime(caseNum)) {
+                answer++;
+                System.out.println(caseNum);
+            }
+        }
+        System.out.println(Arrays.toString(moNum));
         return answer;
     }
     public static void main(String[] args){
         int main_n;
         int main_k;
-        int res;
+        FindTheDecimalNumberInTheKNumber isSolution = new FindTheDecimalNumberInTheKNumber();
         Scanner sc = new Scanner(System.in);
         main_n = sc.nextInt();
         main_k = sc.nextInt();
-        res = solution(main_n, main_k);
-
-        String res1 = k_number(main_n, main_k);
+        int res = isSolution.solution(main_n, main_k);
         System.out.println(res);
-        System.out.println(res1);
+
     }
 }
---
+++ Programmers/Kakao/TestMain.java
@@ -0,0 +1,20 @@
+import Lv2.FindTheDecimalNumberInTheKNumber;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class TestMain {
+    public static void main(String[] args){
+        String s = "110011";
+        String[] splitS = s.split("0");
+        List<String> list = new ArrayList<String>(Arrays.asList(splitS));
+        System.out.println(splitS.getClass().getName());
+        list.removeAll(Collections.singletonList(""));
+        list.
+        Long k = Long.parseLong(s);
+        System.out.println(k);
+        System.out.println(list);
+    }
+}
